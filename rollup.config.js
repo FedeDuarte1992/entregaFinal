0e6a85038daac96af97c42c4397964f48705cbdf
FedeDuarte1992 <124315674+FedeDuarte1992@users.noreply.github.com>
2024-03-17 11:52:14 -0300

    comit de domingo

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/app.js', // Ruta del archivo de entrada principal
+  output: {
+    file: 'dist/bundle.js', // Ruta del archivo de salida
+    format: 'cjs' // Formato de salida (CommonJS, ES Module, UMD, etc.)
+  },
+  plugins: [
+    nodeResolve(), // Resuelve las dependencias de node_modules
+    commonjs(), // Convierte módulos CommonJS en ES6, si es necesario
+    babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }) // Transpila el código con Babel
+  ]
+};
